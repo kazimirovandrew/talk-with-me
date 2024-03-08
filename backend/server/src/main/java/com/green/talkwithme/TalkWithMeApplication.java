fa76caedaf86fa74e9f12f55ce02db24091430af
Andrew <kazimirovandrew@gmail.com>
2024-03-08 17:46:12 +0200

    Init backend project

---
+++ backend/server/src/main/java/com/green/talkwithme/TalkWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.green.talkwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkWithMeApplication.class, args);
+    }
+
+}
